9ee55c2c2aba865c38072fb87e67b79cf1da34bc
roian <adrianroycid@gmail.com>
2024-08-21 22:20:28 +0800

    random number generation and comparing it to input

--- theRustProgrammingLanguage/ch02_ProgrammingAGuessingGame/guessing_game/src/main.rs
+++ theRustProgrammingLanguage/ch02_ProgrammingAGuessingGame/guessing_game/src/main.rs
@@ -1,8 +1,12 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+    println!("The secret number is: {}", secret_number);
     println!("Please input your guess");
 
     let mut guess = String::new();
@@ -10,4 +14,14 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
