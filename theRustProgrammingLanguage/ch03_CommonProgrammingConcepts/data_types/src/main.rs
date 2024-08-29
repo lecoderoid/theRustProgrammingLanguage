396dbc8b0adb0f36179f9dff82e034debfbc54b5
roian <adrianroycid@gmail.com>
2024-08-29 17:24:03 +0800

    floating-point type and numeric ops

---
+++ theRustProgrammingLanguage/ch03_CommonProgrammingConcepts/data_types/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let sum = 5 + 10;
+    let diff = 95.5 - 4.3;
+    let product = 4 * 30;
+    let qoutient = 56.7 / 32.2;
+    let truncated = 10 / 3;
+    let remainder = 43 % 5;
+
+    println!(
+        "sum:{}, difference:{}, product:{}, qoutient:{}, truncated:{}, remainder:{}",
+        sum, diff, product, qoutient, truncated, remainder
+    );
+}
